4b20c58270ef4d9cb5c0499287f6dbca729bff39
Petros <93052465+pkitazos@users.noreply.github.com>
2025-12-04 12:16:33 +0200

    day 4 part 1 - grid map

--- lib/d04.ex
+++ lib/d04.ex
@@ -1,79 +1,44 @@
 defmodule Aoc.D04 do
   alias Aoc.Input
 
-  # @answers %{part1: nil, part2: nil}
-  # def answers, do: @answers
+  @answers %{part1: 1551, part2: nil}
+  def answers, do: @answers
 
   def input(src) do
-    Input.read(__MODULE__, src)
-    |> String.trim()
-    |> String.split("\n", trim: true)
-    |> Enum.map(&String.split(&1, "", trim: true))
+    input =
+      Input.read(__MODULE__, src)
+      |> String.trim()
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.split(&1, "", trim: true))
+
+    {parse_grid(input), length(input), length(hd(input))}
   end
 
-  defp neighbour_rows(idx, range), do: Enum.filter([idx - 1, idx, idx + 1], &(&1 in range))
-  # defp neighbour_rows(idx, range), do: for(dx <- -1..1, (idx + dx) in range, do: idx + dx)
-
-  defp construct_neighbourhood(row_idx, input, row_count, col_count) do
-    range = 0..(row_count - 1)
-
-    rows =
-      row_idx
-      |> neighbour_rows(range)
-      |> Enum.map(&Enum.at(input, &1))
-
-    cond do
-      row_idx == 0 -> [List.duplicate(".", col_count) | rows]
-      row_idx == row_count - 1 -> rows ++ [List.duplicate(".", col_count)]
-      true -> rows
-    end
+  defp parse_grid(input) do
+    for({row, x} <- Enum.with_index(input), {elt, y} <- Enum.with_index(row), do: {{x, y}, elt})
+    |> Enum.into(%{})
   end
 
-  defp neighbourhood_coords(idx, range) do
+  defp neighbour_coords({x, y}) do
     for dx <- -1..1,
-        dy <- 0..2,
-        {dx, dy} != {0, 1},
-        (idx + dx) in range,
-        do: {idx + dx, dy}
+        dy <- -1..1,
+        {dx, dy} != {0, 0},
+        do: {x + dx, y + dy}
   end
 
-  defp process_neighbourhood(row, range) do
-    row
-    |> Enum.at(1)
-    |> Enum.with_index()
-    |> Enum.map(fn
-      {"@", idx} ->
-        idx
-        |> neighbourhood_coords(range)
-        |> Enum.count(fn
-          {x, y} ->
-            row
-            |> Enum.at(y)
-            |> Enum.at(x)
-            |> then(&(&1 == "@"))
-        end)
-        |> then(&(&1 < 4))
+  def part1({grid, row_count, col_count}) do
+    for x <- 0..(row_count - 1),
+        y <- 0..(col_count - 1),
+        Map.get(grid, {x, y}) == "@",
+        reduce: 0 do
+      acc ->
+        neighbour_count =
+          {x, y}
+          |> neighbour_coords()
+          |> Enum.count(fn coord -> Map.get(grid, coord) == "@" end)
 
-      {".", _idx} ->
-        nil
-    end)
-  end
-
-  def part1(input) do
-    row_count = length(input)
-    col_count = length(Enum.at(input, 0))
-
-    out =
-      input
-      |> Enum.with_index()
-      |> Enum.map(&construct_neighbourhood(elem(&1, 1), input, row_count, col_count))
-      |> Enum.map(&process_neighbourhood(&1, 0..(col_count - 1)))
-      |> Enum.map(&Enum.count(&1, fn x -> x end))
-      |> Enum.sum()
-
-    IO.inspect(out)
-
-    :ok
+        if neighbour_count < 4, do: acc + 1, else: acc
+    end
   end
 
   def part2(input) do
