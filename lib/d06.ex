e4a14724abed743e5ec70a45881f4d885d11cd33
Petros <93052465+pkitazos@users.noreply.github.com>
2025-12-06 10:22:05 +0200

    day 6 part 2 - this was just a string parsing exercise?

--- lib/d06.ex
+++ lib/d06.ex
@@ -1,32 +1,99 @@
 defmodule Aoc.D06 do
   alias Aoc.Input
 
-  @answers %{part1: 5_667_835_681_547, part2: nil}
+  @answers %{part1: 5_667_835_681_547, part2: 9_434_900_032_651}
   def answer(1), do: @answers.part1
   def answer(2), do: @answers.part2
 
-  defp parse_string(str) when str in ["*", "+"], do: String.to_atom(str)
+  defp parse_op("*"), do: :*
+  defp parse_op("+"), do: :+
+
+  defp parse_string(str) when str in ["*", "+"], do: parse_op(str)
   defp parse_string(str), do: String.to_integer(str)
 
   defp parse_row(row) do
+    [op | vals] =
+      row
+      |> Enum.reverse()
+      |> Enum.map(&parse_string/1)
+
+    {op, vals}
+  end
+
+  defp transpose(arr), do: Enum.zip_with(arr, &Function.identity/1)
+
+  def parse_input_for_part1(input) do
+    input
+    |> Enum.map(&String.split/1)
+    |> transpose()
+    |> Enum.map(&parse_row/1)
+  end
+
+  defp parse_digits(row) do
     row
-    |> Enum.reverse()
-    |> Enum.map(&parse_string/1)
+    |> Enum.reject(&(&1 == " "))
+    |> Enum.map(&String.to_integer/1)
+    |> Integer.undigits()
+  end
+
+  defp parse_chunk(chunk) do
+    [op_row | val_rows] =
+      chunk
+      |> transpose()
+      |> Enum.reverse()
+
+    vals =
+      val_rows
+      |> Enum.reverse()
+      |> transpose()
+      |> Enum.map(&parse_digits/1)
+
+    op =
+      op_row
+      |> Enum.find(&(&1 != " "))
+      |> parse_op()
+
+    {op, vals}
+  end
+
+  def parse_input_for_part2(input) do
+    max_pad =
+      input
+      |> Enum.map(&String.length/1)
+      |> Enum.max()
+
+    chunk_fun = fn row, acc ->
+      if Enum.all?(row, &(&1 == " ")) do
+        {:cont, Enum.reverse(acc), []}
+      else
+        {:cont, [row | acc]}
+      end
+    end
+
+    after_fun = fn
+      [] -> {:cont, []}
+      acc -> {:cont, Enum.reverse(acc), []}
+    end
+
+    input
+    |> Enum.map(&String.pad_trailing(&1, max_pad, " "))
+    |> Enum.map(&String.graphemes/1)
+    |> transpose()
+    |> Enum.chunk_while([], chunk_fun, after_fun)
+    |> Enum.map(&parse_chunk/1)
   end
 
   def input(src) do
     Input.read(__MODULE__, src)
     |> String.trim()
     |> String.split("\n", trim: true)
-    |> Enum.map(&String.split/1)
-    |> Enum.zip_with(&Function.identity/1)
-    |> Enum.map(&parse_row/1)
+    |> parse_input_for_part2()
   end
 
   defp op(:*, a, b), do: a * b
   defp op(:+, a, b), do: a + b
 
-  defp process_row([op | vals]), do: Enum.reduce(vals, &op(op, &1, &2))
+  defp process_row({op, vals}), do: Enum.reduce(vals, &op(op, &1, &2))
 
   defp parallel_process(input, process_fn) do
     input
@@ -37,8 +104,5 @@ defmodule Aoc.D06 do
 
   def part1(input), do: input |> parallel_process(&process_row/1)
 
-  def part2(input) do
-    input
-    # TODO: Implement part 2
-  end
+  def part2(input), do: input |> parallel_process(&process_row/1)
 end
